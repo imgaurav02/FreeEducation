fc86f23396c79fdc68c16e33ba05ffe511c6616d
Gaurav <imgaurave02@gmail.com>
2021-12-20 14:29:50 +0530

    added db file

---
+++ mymvcblog.sql
@@ -0,0 +1,246 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 20, 2021 at 09:59 AM
+-- Server version: 10.3.16-MariaDB
+-- PHP Version: 7.4.21
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mymvcblog`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `adminType` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `username`, `email`, `password`, `adminType`) VALUES
+(1, 'Gaurav Singh', 'huterabh1@gmail.com', '123', 1),
+(2, 'Aman Singh', 'aman@gmail.com', '123', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comment`
+--
+
+CREATE TABLE `comment` (
+  `id` int(11) NOT NULL,
+  `quesid` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `comment` text NOT NULL,
+  `isVisible` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `comment`
+--
+
+INSERT INTO `comment` (`id`, `quesid`, `name`, `email`, `comment`, `isVisible`) VALUES
+(1, 2, 'Gaurav', 'imgaurave02@gmail.com', 'Hello', 1),
+(2, 2, 'Riturendra', 'imgaurave02@gmail.com', 'Hello', 1),
+(5, 2, 'Gaurav Singh', 'imgaurave02@gmail.com', 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of \"de Finibus Bonorum et Malorum\" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, \"Lorem ipsum dolor sit amet..\", comes from a line in section 1.10.32.\r\n\r\nThe standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from \"de Finibus Bonorum et Malorum\" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.', 1),
+(6, 1, 'Richa Srivastava', 'imgaurave02@gmail.com', '<p>Hello How Are You&nbsp;</p>\r\n\r\n<p>&nbsp;</p>\r\n\r\n<p>I am Gaurav</p>\r\n', 1),
+(7, 2, 'Riturendra', 'imgaurave02@gmail.com', '<h2>Where does it come from?</h2>\r\n\r\n<p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of &quot;de Finibus Bonorum et Malorum&quot; (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, &quot;Lorem ipsum dolor sit amet..&quot;, comes from a line in section 1.10.32.</p>\r\n\r\n<p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from &quot;de Finibus Bonorum et Malorum&quot; by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>\r\n', 1),
+(8, 1, 'Sushant Baghel', 'pkfamily.events@gmail.com', '<p>Hello</p>\r\n', 1),
+(10, 1, 'Richa Srivastava', 'imgaurave02@gmail.com', '<p>dfxgdfg</p>\r\n', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `content`
+--
+
+CREATE TABLE `content` (
+  `id` int(11) NOT NULL,
+  `ques` text NOT NULL,
+  `answer` mediumtext NOT NULL,
+  `class` int(11) NOT NULL,
+  `subject` varchar(255) NOT NULL,
+  `isVisible` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `content`
+--
+
+INSERT INTO `content` (`id`, `ques`, `answer`, `class`, `subject`, `isVisible`) VALUES
+(1, 'This is Question 1', 'And this is the answer', 3, 'math', 1),
+(2, 'This is Question 2', 'And This Is Answer 2 of math', 3, 'math', 1),
+(3, 'Question 1', 'Answer 1', 3, 'math', 1),
+(5, '<p>This Question</p>\r\n', '<p>oldasfnsdkfn</p>\r\n', 3, 'science', 1),
+(6, '<h2>The SQL DELETE Statement</h2>\r\n', '<p>DELETE&nbsp;FROM&nbsp;<em>table_name&nbsp;</em>WHERE&nbsp;<em>condition</em>;</p>\r\n', 5, 'math', 1),
+(7, '<ul>\r\n	<li>helllo test</li>\r\n</ul>\r\n', '<p>yes test done</p>\r\n', 3, 'math', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `meet`
+--
+
+CREATE TABLE `meet` (
+  `id` int(11) NOT NULL,
+  `link` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `meet`
+--
+
+INSERT INTO `meet` (`id`, `link`) VALUES
+(1, 'https://www.google.com');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pdf`
+--
+
+CREATE TABLE `pdf` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `link` varchar(500) NOT NULL,
+  `class` int(11) NOT NULL,
+  `subject` varchar(255) NOT NULL,
+  `isVisible` int(11) NOT NULL DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `pdf`
+--
+
+INSERT INTO `pdf` (`id`, `name`, `link`, `class`, `subject`, `isVisible`) VALUES
+(1, 'Gaurav Resume', 'https://drive.google.com/file/d/12lisrDZ7_kAQKsHYK1dWGzZWm3YocnpQ/view?usp=sharing', 3, 'math', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student`
+--
+
+CREATE TABLE `student` (
+  `id` int(11) NOT NULL,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `class` int(11) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `student`
+--
+
+INSERT INTO `student` (`id`, `first_name`, `last_name`, `email`, `class`, `password`) VALUES
+(9, 'Gaurav', 'Singh', '7007429638', 3, '123');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `comment`
+--
+ALTER TABLE `comment`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `content`
+--
+ALTER TABLE `content`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `meet`
+--
+ALTER TABLE `meet`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `pdf`
+--
+ALTER TABLE `pdf`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `student`
+--
+ALTER TABLE `student`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `comment`
+--
+ALTER TABLE `comment`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `content`
+--
+ALTER TABLE `content`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `meet`
+--
+ALTER TABLE `meet`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `pdf`
+--
+ALTER TABLE `pdf`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `student`
+--
+ALTER TABLE `student`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
